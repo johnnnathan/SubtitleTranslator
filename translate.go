0220a8a3dce41e3a48f4d6364981a3fdf468ed69
johnnnathan <johnnnathan@proton.me>
2024-09-09 14:19:32 +0200

    Finished file creation

--- translate.go
+++ translate.go
@@ -4,25 +4,24 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"github.com/bregydoc/gtranslate"
 	"log"
 	"os"
 	"strings"
-	"github.com/bregydoc/gtranslate"
 )
 
-
 var (
-	destLanguage string
-  sourceLanguage string 
+	destLanguage   string
+	sourceLanguage string
 )
 
 func main() {
 	intermediateSourceLanguage := flag.String("srclang", "en", "Source language (e.g., 'en' for English)")
-	intermediateDestinationLanguage := flag.String("destlang", "gr", "Destination language (e.g., 'en' for English)")
+	intermediateDestinationLanguage := flag.String("destlang", "el", "Destination language (e.g., 'en' for English)")
 
 	flag.Parse()
 
-	sourceLanguage = strings.ToLower(*intermediateSourceLanguage)  // Dereference the pointer
+	sourceLanguage = strings.ToLower(*intermediateSourceLanguage)    // Dereference the pointer
 	destLanguage = strings.ToLower(*intermediateDestinationLanguage) // Dereference the pointer
 
 	// Print out the parsed language codes
@@ -30,21 +29,21 @@ func main() {
 	fmt.Printf("Destination Language: %s\n", destLanguage)
 	// Read the file
 
-  print("Something")
-	text := readFile("/home/dimi/Desktop/Projects/SubtitleTranslator/template.srt")
+	text , booleanMap:= readFile("/home/dimi/Desktop/Projects/SubtitleTranslator/template.srt")
 
-  print("Something")
 	// Translate the text
-	translatedText := translateText(text)
+	translatedText := translateText(text, booleanMap)
 
 	// Output the translated text
 	for _, element := range translatedText {
 		fmt.Println(element)
 	}
+  writeFile(translatedText)
 }
 
 // Read the file and extract strings for translation
-func readFile(path string) []string {
+func readFile(path string) ([]string, []bool) {
+	var booleanMapOfLines []bool
 	var preTranslationStrings []string
 	file, err := os.Open(path)
 	if err != nil {
@@ -57,9 +56,12 @@ func readFile(path string) []string {
 	previousEmpty = false
 	for scanner.Scan() {
 		text := scanner.Text()
+    preTranslationStrings = append(preTranslationStrings, text)
 		if previousEmpty || (text != "\n" && (len(text) >= 2 && text[2] != ':')) {
-			preTranslationStrings = append(preTranslationStrings, text)
-		}
+      booleanMapOfLines = append(booleanMapOfLines, true)
+		}else{
+      booleanMapOfLines = append(booleanMapOfLines, false)
+    }
 		if text == "\n" {
 			previousEmpty = true
 			continue
@@ -69,16 +71,33 @@ func readFile(path string) []string {
 	if err := scanner.Err(); err != nil {
 		log.Fatalf("Failed to read file: %v", err)
 	}
-	return preTranslationStrings
+	return preTranslationStrings, booleanMapOfLines
 }
 
 // Translate the text
-func translateText(text []string) []string {
+func translateText(text []string, booleanmap []bool) []string {
 	var translatedText []string
-	for _, element := range text{
-    translatedLine , _ := gtranslate.TranslateWithParams(element, gtranslate.TranslationParams{From: sourceLanguage , To: destLanguage},) 
-    translatedText = append(translatedText, translatedLine)
+	for index, element := range text {
+    if booleanmap[index] == true{
+      if element != ""{
+        translatedLine, _ := gtranslate.TranslateWithParams(element, gtranslate.TranslationParams{From: sourceLanguage, To: destLanguage})
+        translatedText = append(translatedText, translatedLine)
+      }
+      continue
+    }
+    translatedText = append(translatedText, element)
 	}
 
 	return translatedText
+}
+
+func writeFile(translatedText []string) {
+  f, _ := os.Create("Translation.srt")
+  defer f.Close()
+  for _, line := range translatedText{
+    _, err := f.WriteString(line + "\n")
+    if err != nil{
+      log.Fatal(err)
+    }
+  }
 }
